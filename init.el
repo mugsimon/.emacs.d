c376b9499736ae6b108c011a1210656860388295
mugsimon <mug24375826x00a@outlook.jp>
2025-04-30 16:08:44 +0900

    Add git-gutter

--- init.el
+++ init.el
@@ -622,6 +622,20 @@
   :ensure t
   :config
   (treemacs-load-theme "nerd-icons"))
+
+(use-package git-gutter
+  :ensure t
+  :hook (prog-mode . git-gutter-mode)
+  :custom
+  (git-gutter:modified-sign "~")
+  (git-gutter:added-sign    "+")
+  (git-gutter:deleted-sign  "-")
+  :custom-face
+  ;; (git-gutter:modified ((t (:background "#f1fa8c"))))
+  ;; (git-gutter:added    ((t (:background "#50fa7b"))))
+  ;; (git-gutter:deleted  ((t (:background "#ff79c6"))))
+  )
+
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 ;;; @ GC Threshold                                                  ;;;
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
